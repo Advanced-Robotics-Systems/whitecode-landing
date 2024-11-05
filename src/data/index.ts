7a6df234b55c488cb5fe157ddbab1b1d408d5258
A1-mamun <1154almamun@gmail.com>
2024-11-05 13:37:21 +0600

    Privacy Policy data added

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,5 @@
 import { ICONS } from "@/utils/icons";
+import { title } from "process";
 
 // Navbar Links
 export const navLinks = [
@@ -417,3 +418,207 @@ export const careerData = [
     date: "2024-10-17",
   },
 ];
+
+export const privacyPolicy = {
+  title: "Privacy Policy (Australia)",
+
+  sections: [
+    {
+      title: "Privacy Policy",
+      lisr: {},
+      introduction: [
+        "Project WhiteCode is committed to the protection of your personal information. We understand our relationship with you is based on trust. We care about giving you the best private web browsing experience. We treat your personal data confidentially and in accordance with the data protection legislation and this Privacy Policy.",
+        "The use of our website is usually possible without providing personal information. Where any personal data (such as name, address or email address) is provided, this is as far as possible on a voluntary basis. These data are not without your express consent used for any other purpose.",
+        "We point out that data transmission over the Internet (for example, when communicating via e-mail) can have security gaps. A complete protection of data against unauthorised access by third parties is impossible.",
+      ],
+      subSections: [
+        {
+          title: "1. What Information We Collect",
+          subtitle:
+            "We may collect the following types of personal information:",
+          content: [
+            "Personal Identifiable Information (PII): Your name, email address, phone number, and mailing address when you submit forms or sign up for services.",
+            "Technical Information: IP address, browser type, and usage data through cookies and analytics tools.",
+            "Financial Information: Credit card or payment details for transactions.",
+          ],
+        },
+        {
+          title: "2. How We Collect Your Information",
+          subtitle:
+            "We may collect personal information directly from you through:",
+          content: [
+            "Forms you submit on our website",
+            "Your subscription to our newsletters or other services",
+            "Contact forms or customer service inquiries",
+            "We also use cookies, web beacons, and analytics tools to gather usage data. You can control or disable cookies via your browser settings.",
+          ],
+        },
+        {
+          title: "3. How We Use Your Information",
+          subtitle:
+            "We collect and use your personal information for purposes including, but not limited to:",
+          content: [
+            "Providing our services to you",
+            "Processing transactions",
+            "Responding to your inquiries",
+            "Improving our website and customer experience",
+            "Marketing purposes (with your consent)",
+          ],
+        },
+        {
+          title: "4. Disclosure of Your Information",
+          subtitle: "We may share your personal information with:",
+          content: [
+            "Service providers (e.g., IT services, payment processors)",
+            "Government or law enforcement agencies when required by law",
+            "Third parties as authorized by you or as required by law",
+          ],
+        },
+        {
+          title: "5. Security of Your Information",
+          subtitle:
+            "We take reasonable steps to safeguard your personal information from unauthorized access, use, or disclosure. However, no method of transmission over the internet is 100% secure, and we cannot guarantee absolute security.",
+          content: [],
+        },
+        {
+          title: "6. Your Rights",
+          subtitle: "Under the Privacy Act 1988, you have the right to:",
+          content: [
+            "Access your personal information",
+            "Request corrections to your personal information",
+            "Lodge a complaint if you believe your privacy has been breached",
+            "To exercise your rights, please contact us at [Insert Contact Email/Phone Number].",
+          ],
+        },
+        {
+          title: "7. Third-Party Links",
+          subtitle:
+            "Our website may contain links to third-party websites. We are not responsible for the privacy practices of those websites. We encourage you to read their privacy policies.",
+          content: [],
+        },
+        {
+          title: "8. Changes to This Policy",
+          subtitle:
+            "We may update this Privacy Policy from time to time to reflect changes in our practices. The updated policy will be available on this page with the date of the last revision.",
+          content: [],
+        },
+      ],
+    },
+    {
+      title: "Security",
+      lisr: {},
+      introduction: [
+        "We understand the importance of your personal information and have appropriate procedures in place to safeguard and secure the information we collect to prevent unauthorised access or disclosure, maintain data accuracy and ensure the appropriate use of information. However, we point out that data transmission over the Internet (for example, when communicating via email) can have security gaps. A complete protection of data against unauthorised access by third parties is impossible.",
+        "We take website and credit card security extremely seriously and always endeavour to provide a secure, safe platform from which to conduct online transactions. To ensure you are accessing a secure server, look for the unbroken key or closed lock symbol located either at the bottom left or top right of your browser window. If it appears, then SSL is active. You can double-check by looking at the URL. If SSL is active, then the first characters of the URL will read 'https' rather than just 'http'. It is important for you to protect against unauthorised access to your password and your computer.",
+        "Ensure you sign out when you have finished visiting our websites especially if you have accessed the websites from a shared computer.",
+      ],
+      subSections: [],
+    },
+    {
+      title: "Server Log Files",
+      list: {
+        title:
+          "The provider of these pages automatically collects and stores information which your browser automatically sends to us in server log files.The information stored includes –",
+        listItem: [
+          "Browser type, version and operating system",
+          " Referrer URL",
+          "Host name and IP address of accessing computer",
+          "Time of server request",
+        ],
+      },
+      introduction: [
+        "This data is not specific to individuals. A compilation of this data with other data sources is not made. We reserve the right to examine this data retrospectively when concrete evidence of unlawful use are known to us.",
+      ],
+    },
+    {
+      title: "Newsletter Data",
+      lisr: {},
+      introduction: [
+        "If you want to receive the newsletter offered on the website, we need your email address and information that allow us to verify that you are the owner of the e-mail address and agree to receive the newsletter. Further data is not collected. We use this information only to send the requested information.",
+        "The consent to the storage of the data, the e-mail address as well as their use for sending the newsletter can be revoked at any time, for example through the 'unsubscribe' link in the newsletter.",
+      ],
+      subSections: [],
+    },
+    {
+      title: "Wishlist Data",
+      lisr: {},
+      introduction: [
+        "By filling in your email address we can provide an extended functionality of your personal Wishlist and save it for the next time log in at our website. Further data is not collected as we use your email only to be able to store your Wishlist.",
+      ],
+    },
+    {
+      title: "Information, Erasure, Blocking",
+      lisr: {},
+      introduction: [
+        "You always have the right to free information about your stored personal data, its origin and recipients and the purpose of the data processing and the right to correct, block or delete this data. For this purpose, as well as other questions relating to personal data you can contact at us any time at the address stated in the imprint.",
+      ],
+    },
+    {
+      title: "Contact Form",
+      lisr: {},
+      introduction: [
+        "If you send us enquiries via contact requests, your data out of the request form (including contact information you provide there) will be stored for the purpose of processing the request only and for the purpose of follow-up questions. This data will not be disclosed without your consent.",
+      ],
+    },
+    {
+      title: "Rejection of Promotional Emails",
+      lisr: {},
+      introduction: [
+        "The use of contact date published under the obligatory imprint information for sending unsolicited advertisements and information materials is hereby rejected. The operators of the sites expressly legal steps in case of sending unsolicited promotional information, including spam emails.",
+      ],
+    },
+    {
+      title: "PayPal",
+      list: {
+        title:
+          "For payment via 'PayPal' we use the following service provider –",
+        listItem: [
+          "PayPal (Europe) S.à r.l. et Cie, S.C.A",
+          "22-24 Boulevard Royal L-2449 Luxembourg",
+        ],
+      },
+      introduction: [
+        "We only use payment service providers that are certified according to PCI DSS* and thus fulfil the strictest requirements for the secure handling and storage of credit card data.",
+        "*(PCI DSS is the abbreviation for 'Payment Card Industry Data Security Standard' and is based on the security programs Visa AIS (Account Information Security) and MasterCard SDP (Site Data Protection).",
+        "Apart from this, we do not pass on personal data to third parties without your request, nor sell or rent out data. However, we reserve the right to disclose information about you if we are legally obliged to do so, or if the publication of lawfully acting authorities or law enforcement agencies is required by us.",
+      ],
+    },
+    {
+      title: "Privacy Statement for the Use of Google Analytics",
+      lisr: {},
+      introduction: [
+        "This website uses functions of the web analytics service Google Analytics. Provider is Google Inc. 1600 Amphitheatre Parkway Mountain View, CA 94043.",
+        "Google Analytics uses 'Cookies'. These are text files that are stored on your computer and enable analysis of your use of the website. The information generated by the cookie about your use of this website are usually transferred to a Google server in the USA and stored there.",
+        "In case of activation of the IP anonymisation on this website your IP address from Google will be truncated within Member States of the European Union or other parties to the Agreement on the European Economic Area. Only in exceptional cases will the full IP address transferred to a Google server in the USA and shortened there. On behalf of the operator of this website Google will use this information to evaluate your use of the website, compiling reports on website activity and providing other with website and internet related services to the website operator. Google Analytics will not merge your IP address with other data from Google.",
+        "You may refuse the use of cookies by adjusting your browser software; however, we point out that in this case you may not be able to use the full extent and not all features of this website.",
+        "You can also prevent the data generated by the cookie about your use of the website (incl. Your IP address) to Google and the processing of these data by Google, by downloading and installing from the following link the browser plugin 'https://tools.google.com/dlpage/gaoptout'",
+      ],
+    },
+    {
+      title: "Privacy Statement for the Use of Facebook",
+      lisr: {},
+      introduction: [
+        "On our pages are incorporated plugins of the social network Facebook, provider Facebook Inc. 1 Hacker Way, Menlo Park, California 94025, USA.",
+        "The Facebook plugin you recognise by the Facebook logo or the 'Like Button' ('Like') on our side. An overview of the Facebook plugins can be foundat 'https://developers.facebook.com/docs/plugins'",
+        "When you visit our pages, the plugin establishes a direct connection between your browser and the Facebook server. Facebook receives the information that you have with your IP address visited our page. If you click on the Facebook 'Like' button while you are logged into your Facebook account, you can link the content of our pages on your Facebook profile.This allows Facebook to assign your visit to our pages to your user account. We point out that we as providers of the sites have no knowledge of the content of the transmitted data and how Facebook will use the data. For more information, please see the privacy policy of Facebook at 'https://facebook.com/policy.php' ",
+        "Please log out from your Facebook account if you do not wish that Facebook receives data relating to visiting our pages.",
+      ],
+    },
+    {
+      title: "Privacy Policy for use of Instagram",
+      lisr: {},
+      introduction: [
+        "Our site uses functions of the service Instagram. These functions are offered by Instagram Inc. 1601 Willow Road, Menlo Park, CA 94025, USA.",
+        "When you are logged into your Instagram account (you can by clicking on the Instagram buttons) then it is possible that content of our website be linked to your Instagram profile. This allows Instagram to assign your visit to our pages to your user account. We point out that we as providers of the sites possess no knowledge of the content of the transmitted data and their use through Instagram.For more information, please see the privacy policy of Instagram at 'https://instagram.com/about/legal/privacy'",
+      ],
+    },
+    {
+      title: "Privacy Policy for use of Instagram",
+      lisr: {},
+      introduction: [
+        "Our site uses functions of the network LinkedIn. Provider is the LinkedIn Corporation 2029 Stierlin Court, Mountain View, CA 94043, USA.",
+        "Every time you visit a page containing functions of LinkedIn, a connection to servers of LinkedIn is established. LinkedIn is informed that you have visited our website with your IP address. If you click the 'Recommend' button of LinkedIn and are logged into your account with LinkedIn, then it is possible for LinkedIn to assign your visit to our website to your LinkedIn user account. We point out that we as providers of the sites have no knowledge of the content of the transmitted data and their use by LinkedIn. More information can be found in the Privacy Policy LinkedIn at 'https://www.linkedin.com/legal/privacy-policy'",
+      ],
+    },
+  ],
+};
--- src/sections/about/policy/policy-austrailia.tsx
+++ src/sections/about/policy/policy-austrailia.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const PolicyAustrailia = () => {
-  return (
-    <div>PolicyAustrailia</div>
-  )
-}
+  return <div>PolicyAustrailia</div>;
+};
 
-export default PolicyAustrailia
+export default PolicyAustrailia;
