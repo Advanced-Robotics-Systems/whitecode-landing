802fa2b23a8c86437931d22a9f885807cd4a2981
A1-mamun <1154almamun@gmail.com>
2024-11-21 08:38:00 +0600

    service and mission section link added and solution section text changed

--- src/sections/home/mission2.tsx
+++ src/sections/home/mission2.tsx
@@ -2,11 +2,9 @@
 import { ICONS } from "@/utils/icons";
 import { motion } from "framer-motion";
 import { Button, Image } from "@nextui-org/react";
+import Link from "next/link";
 
 const Mission = () => {
-  const handleAboutUsClick = () => {
-    window.open(`http://localhost:3000/about`);
-  };
   return (
     <section className="bg-sky-15 padding-responsive">
       <article className="flex flex-col md:flex-row-reverse gap-12 lg:gap-16 xl:gap-20 items-center font-jura bg-white px-5 py-7 md:px-7 md:py-10 rounded-2xl">
@@ -14,7 +12,7 @@ const Mission = () => {
           initial={{ opacity: 0, x: -20 }}
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6 }}
-          className="lg:w-2/5 space-y-8 md:space-y-10 lg:space-y-16"
+          className="lg:w-2/5 "
         >
           <header className="space-y-3 md:space-y-4 lg:space-y-5">
             <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-medium text-light-indigo">
@@ -27,21 +25,21 @@ const Mission = () => {
               of your business.
             </p>
           </header>
-
-          <Button
-            radius="full"
-            size="md"
-            variant="ghost"
-            className="border-1 border-black hover:!bg-transparent hover:scale-105"
-            endContent={ICONS.arrow_right}
-            onPress={handleAboutUsClick}
-          >
-            <div className=" py-4">
-              <span className="text-base md:text-xl lg:text-2xl">
-                Aboout Us
-              </span>
-            </div>
-          </Button>
+          <Link href="/about">
+            <Button
+              radius="full"
+              size="md"
+              variant="ghost"
+              className="border-1 border-black hover:!bg-transparent hover:scale-105 mt-8 md:mt-10 lg:mt-16"
+              endContent={ICONS.arrow_right}
+            >
+              <div className=" py-4">
+                <span className="text-base md:text-xl lg:text-2xl">
+                  Aboout Us
+                </span>
+              </div>
+            </Button>
+          </Link>
         </motion.aside>
 
         <motion.figure
