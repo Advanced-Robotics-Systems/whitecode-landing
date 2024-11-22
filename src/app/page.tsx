058b43b2ca2fb8377cbd3ab3e270e2daa2a45afb
Ishmum <ishmum20000@gmail.com>
2024-11-23 04:57:19 +0800

    Add address in schema data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,19 +75,27 @@ export default function Home() {
             "url": "https://prowhitecode.com",
             "logo": "https://prowhitecode.com/favicon.png",
             "sameAs": [
-              "https://www.facebook.com/YourPage",
-              "https://twitter.com/YourHandle",
+              "https://www.facebook.com/profile.php?id=61566845451708",
+              "https://www.instagram.com/prowhitecodeofficial.au/",
               "https://www.linkedin.com/company/project-whitecode/"
             ],
             "description":
               "Project WhiteCode offers custom software and app development services tailored to businesses worldwide.",
+							"address": {
+							"@type": "PostalAddress",
+							"streetAddress": "576 Flinders Lane",
+							"addressLocality": "Melbourne",
+							"addressRegion": "VIC",
+							"postalCode": "3000",
+							"addressCountry": "Australia"
+						},
             "contactPoint": {
               "@type": "ContactPoint",
-              "telephone": "+880 1622-257180",
+              "telephone": "+61 414 452 281",
               "contactType": "Customer Service",
               "areaServed": "Worldwide",
               "availableLanguage": ["English"]
-            }
+            },
           }),
         }}
       />
