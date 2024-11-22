3276a90c2681136c7e08a13df9b8e1dcb4e8bb5c
Ishmum <ishmum20000@gmail.com>
2024-11-22 22:01:58 +0800

    Update metadata for work

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,7 +1,47 @@
 import { ImageCarousel } from "@/components";
 import { carouselProjects } from "@/data";
 import { AllWorks, Chat, WorkHero } from "@/sections";
-import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Our Work | Project WhiteCode - Innovative Software Solutions",
+  description:
+    "Explore the portfolio of Project WhiteCode. See our innovative software solutions, including web and mobile apps, enterprise systems, and custom AI projects.",
+  keywords:
+    "Project WhiteCode portfolio, software development portfolio, web application projects, mobile app projects, enterprise software solutions, custom AI solutions, innovative software projects, successful client work",
+  openGraph: {
+    title: "Our Work | Project WhiteCode - Software Development Portfolio",
+    description:
+      "Discover the exceptional software solutions crafted by Project WhiteCode. Explore our portfolio of innovative projects, from web and mobile applications to enterprise systems.",
+    url: "https://prowhitecode.com/our-work",
+    siteName: "Project WhiteCode",
+    images: [
+      {
+        url: "/preview.png", // Need to replace with page ss
+        width: 1200,
+        height: 630,
+        alt: "Project WhiteCode Portfolio - Innovative Software Projects",
+        type: "image/png",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Our Work | Project WhiteCode - Portfolio of Innovative Software Solutions",
+    description:
+      "See how Project WhiteCode delivers exceptional software solutions. Explore our portfolio of web, mobile, and custom software projects tailored to client needs.",
+    images: ["/preview.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://prowhitecode.com/our-work",
+  },
+};
 
 const Work = () => {
 	return (
