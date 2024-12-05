432bc8e4e23711c783897b6abeacdd0c61cae8db
Ishmum <ishmum20000@gmail.com>
2024-12-05 13:41:52 +0800

    Update config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,21 +12,13 @@ const config: Config = {
   theme: {
     screens: {
       xxs: "400px",
-
       xs: "480px",
-
       sm: "640px",
-
       md: "768px",
-
       lg: "1024px",
-
       xl: "1280px",
-
       "2xl": "1536px",
-
       "3xl": "1700px",
-
       "4xl": "1880px",
     },
     fontSize: {
