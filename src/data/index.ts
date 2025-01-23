70097fbd76423ffa93d8172b130f980611503c53
A1-mamun <1154almamun@gmail.com>
2025-01-23 16:17:40 +0600

    Finland out from map and reviews autoplay

--- src/data/index.ts
+++ src/data/index.ts
@@ -330,7 +330,6 @@ export const countries: any = {
     address: "576 Flinders Lane Melbourne VIC 3000",
     hq: "Global HQ",
   },
-  Finland: { name: "Finland", address: "Helsinki, Finland 00100" },
   Bangladesh: {
     name: "Bangladesh",
     address: "Oasis, House 47, Road 3A, Dhanmondi, Dhaka - 1205",
@@ -672,7 +671,7 @@ export const clientsData = [
     img: "/images/clients/kone.png",
   },
   {
-    name: "The Guestbook",
+    name: "The Guest Book",
     img: "/images/clients/guest-book.png",
   },
   {
@@ -927,39 +926,46 @@ export const servicesData = [
 // Our reviews data
 export const reviewsData = [
   {
-    name: "Alex Mulder",
-    designation: "Founder USG People",
-    img: "/alex.png",
+    name: "Harmonya",
+    img: "/images/clients/harmonya.png",
     review:
-      "“Throughout my career as a CEO, entrepreneur and investor, I have rarely worked with anyone who has impressed me greatly as an entrepreneur, negotiator and strategist. Except Chris Zadeh, who has proven to be very successful an capable in all these areas.Moreover, he keeps his word and is 100% reliable.”",
+      "We required a digital presence that accurately represented our goal as a company working to transform AI-driven retail intelligence. That is just what Project WhiteCode provided: a highly practical, aesthetically pleasing, and user-friendly platform that increases user engagement. Throughout the entire process, their meticulousness and dedication to quality were apparent. We're blown away!",
   },
   {
-    name: "Alex Mulder",
-    designation: "Founder USG People",
-    img: "/alex.png",
+    name: "Arkken",
+    img: "/images/clients/arkken.png",
     review:
-      "“Throughout my career as a CEO, entrepreneur and investor, I have rarely worked with anyone who has impressed me greatly as an entrepreneur, negotiator and strategist. Except Chris Zadeh, who has proven to be very successful an capable in all these areas.Moreover, he keeps his word and is 100% reliable.”",
+      "Project WhiteCode's partnership changed everything for us. Our design aesthetic was Scandinavian, and we needed a website that was sleek, contemporary, and incredibly responsive. The team surpassed our expectations by creating a visually appealing and intuitive UI that appeals to our target population. The difference was entirely due to their professionalism and inventiveness",
   },
   {
-    name: "Alex Mulder",
-    designation: "Founder USG People",
-    img: "/alex.png",
+    name: "The Guest Book",
+    img: "/images/clients/guest-book.png",
     review:
-      "“Throughout my career as a CEO, entrepreneur and investor, I have rarely worked with anyone who has impressed me greatly as an entrepreneur, negotiator and strategist. Except Chris Zadeh, who has proven to be very successful an capable in all these areas.Moreover, he keeps his word and is 100% reliable.”",
+      "We wanted to develop a smooth and entertaining reward system for passengers, and Project WhiteCode enabled us to realize that goal. The website they created is very user-friendly in addition to being visually appealing. Our clients' interactions with our services have been greatly impacted by their proficiency in branding and UI/UX. It makes us very happy.",
   },
   {
-    name: "Alex Mulder",
-    designation: "Founder USG People",
-    img: "/alex.png",
+    name: "WorkHero",
+    img: "/images/clients/work-hero.png",
     review:
-      "“Throughout my career as a CEO, entrepreneur and investor, I have rarely worked with anyone who has impressed me greatly as an entrepreneur, negotiator and strategist. Except Chris Zadeh, who has proven to be very successful an capable in all these areas.Moreover, he keeps his word and is 100% reliable.”",
+      "We needed a website that showcased our automation solutions in a compelling and user-friendly way, and Project WhiteCode nailed it. The clean design, smooth animations, and strategic layout made our platform more engaging and accessible to clients. The team was incredibly responsive and professional throughout the process. Highly recommended!",
   },
   {
-    name: "Alex Mulder",
-    designation: "Founder USG People",
-    img: "/alex.png",
+    name: "Fruitful",
+    img: "/images/clients/fruitful.png",
     review:
-      "“Throughout my career as a CEO, entrepreneur and investor, I have rarely worked with anyone who has impressed me greatly as an entrepreneur, negotiator and strategist. Except Chris Zadeh, who has proven to be very successful an capable in all these areas.Moreover, he keeps his word and is 100% reliable.”",
+      "Project WhiteCode took our concept and turned it into a digital experience that embodies the environmental commitment of our brand. Our consumers can easily connect with us because of the design's current, fresh, and very functional features. The procedure went smoothly because of their knowledge and commitment. 'A wonderful group to collaborate with.'",
+  },
+  {
+    name: "Seven Stars",
+    img: "/images/clients/seven-stars.png",
+    review:
+      "As a company in the gaming industry, we needed a website that was dynamic, interactive, and visually engaging. Project WhiteCode delivered exactly that! From the immersive design to the seamless navigation, everything was crafted with precision. They truly understand how to blend aesthetics with functionality. We're thrilled with the results!",
+  },
+  {
+    name: "JAREC",
+    img: "/images/clients/jarec.png",
+    review:
+      "We recently partnered with Project WhiteCode for our website development, and we're thrilled with the results. Their team showcased exceptional expertise, delivering a site that excels in design, usability, and performance. From start to finish, the process was seamless and professional. We're excited to collaborate with them on future projects!",
   },
 ];
 
